9b3334abdbfa856a303406b6f2f35438435265b3
fathimohamed <mohafathi24@gmail.com>
2024-06-23 15:21:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const name = "Joe";
-const height = 74;
+const { expect } = require("chai");
+
+const name = "Susan";
+const height = 39;
+
+
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+describe('Number Validation', function() { it('height should be greater than 0 and less than 40', function() { expect(height).to.be.greaterThan(0); expect(height).to.be.lessThan(40); }); it('message should be correct', function() { expect(message).to.equal('Susan is 39 inches tall'); }); });
